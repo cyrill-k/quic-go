d04cf507d3d9c6e35596159b1fcc022ddcc43506
Zero King <l2dy@icloud.com>
2020-06-22 04:52:15 +0000

    make DialEarly return EarlySession
    Fixes https://github.com/lucas-clemente/quic-go/issues/2619.

--- client.go
+++ client.go
@@ -138,7 +138,7 @@ func DialEarly(
 	host string,
 	tlsConf *tls.Config,
 	config *Config,
-) (Session, error) {
+) (EarlySession, error) {
 	return dialContext(context.Background(), pconn, remoteAddr, host, tlsConf, config, true, false)
 }
 
