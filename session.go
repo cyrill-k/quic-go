bc0094bbcd7a4bcbb0f16e0572dc50a4d3e6401b
Marten Seemann <martenseemann@gmail.com>
2020-11-06 14:16:11 +0700

    allow an amplification factor of 3.x

--- session.go
+++ session.go
@@ -1573,7 +1573,7 @@ func (s *session) sendPacket() (bool, error) {
 
 	if !s.handshakeConfirmed {
 		now := time.Now()
-		packet, err := s.packer.PackCoalescedPacket(s.sentPacketHandler.AmplificationWindow())
+		packet, err := s.packer.PackCoalescedPacket()
 		if err != nil || packet == nil {
 			return false, err
 		}
